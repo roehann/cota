e58cd5f07c68bbfd41c2de979a4835b613e3d4ef
Hannes <h.roenn@stud.hs-wismar.de>
2024-02-13 20:53:24 +0100

    init

---
+++ another.py
@@ -0,0 +1,6 @@
+def add(a, b):
+    return a + b
+
+def hello():
+    a = 'hello'
+    return a
---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a unique file 🤓
+
+import another
+import time
+
+
+def main():
+    data = another.hello()
+    print(data)
+    print("This works ...")
+    time.sleep(2)
