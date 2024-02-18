d1c3766ea7760db44d95f664349b37144be168e8
Hannes R <72553816+roehann@users.noreply.github.com>
2024-02-18 14:55:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import wifi
 import ota
-from custom import say_hello
+import wifi
 
 
 def conn_wifi(ssid: str, password: str):
@@ -12,7 +11,6 @@ def conn_wifi(ssid: str, password: str):
 def main():    
     settings = ota.get_thingsboard_settings()
     conn_wifi(settings["wifi_ssid"], settings["wifi_password"])
-    
     tb_ota = ota.OverTheAirUpdate(tb_url=settings["thingsboard_url"], 
                                   tb_port=settings["thingsboard_port"], 
                                   tb_device_access_token=settings["thingsboard_device_token"])
@@ -20,15 +18,16 @@ def main():
     while True:
         try:
             if tb_ota.is_new_firmware_available():
+                # New firmware is available, let's download it.
                 tb_ota.download_firmware_files()
             else:
-                # Custom code comes here
-                say_hello()  
+                # Add your custom code here.
+                pass
         except ConnectionError as e:
-            # Custom code comes here 
+            # Handle request connection errors here, for example you might try to reconnect to Wi-Fi (Optional).
             pass   
         except ota.OverTheAirUpdateError as e:
-            # Custom code comes here
+            # Handle exceptions related to the firmware download process (Optional).
             pass   
 
 
